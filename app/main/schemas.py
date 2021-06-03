6283e3659f725fd7928cd8590b0bffef6b734250
david mwangi <njorogedavid13@gmail.com>
2021-06-03 15:39:18 +0300

    initial folder structure

--- app/main/schemas.py
+++ app/main/schemas.py
@@ -1,7 +1,14 @@
 from app import ma
+from app.main.models import User
 
 
-class UserSchema(ma.Schema):
+class UserSchema(ma.SQLAlchemySchema):
     class Meta:
         # Fields to expose
-        fields = ("email", "date_created")
+        model = User
+
+    id = ma.auto_field()
+    name = ma.auto_field()
+    email = ma.auto_field()
+    date_created = ma.auto_field()
+    status = ma.auto_field()
