98fe9b831010a6dff59ef8db3dac05955a78951e
david mwangi <njorogedavid13@gmail.com>
2021-10-23 17:11:08 +0300

    add testing configurations

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
 
 class DefaultConfig:
+    # Default for localhost
     # Statement for enabling the development environment
     DEBUG = True
 
@@ -12,12 +13,6 @@ class DefaultConfig:
     # SQLite for this example
     SQLALCHEMY_DATABASE_URI = 'postgresql://postgres_user:password@localhost:5438/postgres'
     DATABASE_CONNECT_OPTIONS = {}
-
-    # Application threads. A common general assumption is
-    # using 2 per available processor cores - to handle
-    # incoming requests using one and performing background
-    # operations using the other.
-    THREADS_PER_PAGE = 2
 
     # Enable protection agains *Cross-site Request Forgery (CSRF)*
     CSRF_ENABLED = True
@@ -42,15 +37,8 @@ class HerokuConfig:
     BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
     # Define the database - we are working with
-    # SQLite for this example
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     DATABASE_CONNECT_OPTIONS = {}
-
-    # Application threads. A common general assumption is
-    # using 2 per available processor cores - to handle
-    # incoming requests using one and performing background
-    # operations using the other.
-    THREADS_PER_PAGE = 2
 
     # Enable protection agains *Cross-site Request Forgery (CSRF)*
     CSRF_ENABLED = True
